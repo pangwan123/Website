9c3df520f645faa79dd302e1c02075f7aaffd272
pangwan123 <wizdude123@gmail.com>
2015-12-06 16:31:44 -0500

    first commit

---
+++ client/routers.js
@@ -0,0 +1,31 @@
+Router.route('/', {
+    template: 'about',
+    title: 'CTC - about'
+});
+
+Router.route('/gallery', {
+    template: 'gallery',
+    title: 'CTC - gallery'
+});
+
+
+Router.route('/contact', {
+    template: 'contact',
+    title: 'CTC - contact'
+});
+
+Router.route('/converter', {
+	template: 'converter',
+	title: 'CTC - converter'
+});
+
+
+Router.route('/math', {
+	template: 'math',
+	title: 'CTC - math'
+});
+
+Router.route('/login', {
+    template: 'login',
+    title: 'CTC - login'
+});
